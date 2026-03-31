a85a38c162277bc3e7e566be51354c77011675cc
Bhavya-India <bhavyabhanusriadusumalli@gmail.com>
2026-03-31 20:39:36 +0530

    Time: 3 ms (61.46%), Space: 24.4 MB (9.91%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+using namespace std;
+
+class MinStack {
+private:
+    stack<int> s;
+    stack<int> minS;
+
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        
+        if (minS.empty() || val <= minS.top())
+            minS.push(val);
+        else
+            minS.push(minS.top());
+    }
+    
+    void pop() {
+        s.pop();
+        minS.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minS.top();
+    }
+};
