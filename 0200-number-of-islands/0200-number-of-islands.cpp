0406b00a1892c0a947ac05d17779b2469baafc9d
Bhavya-India <bhavyabhanusriadusumalli@gmail.com>
2026-05-28 18:09:00 +0530

    Time: 25 ms (72.47%), Space: 16.1 MB (96.1%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+
+        // Boundary check
+        if (row < 0 || col < 0 ||
+            row >= grid.size() ||
+            col >= grid[0].size() ||
+            grid[row][col] == '0') {
+            return;
+        }
+
+        // Mark visited land as water
+        grid[row][col] = '0';
+
+        // Visit all 4 directions
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+
+        int islands = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+
+                // Found an island
+                if (grid[i][j] == '1') {
+                    islands++;
+
+                    // Remove connected land
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return islands;
+    }
+};
